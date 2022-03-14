822fdd5b651ce4201913b34086bae3f7e8d0fcb2
Oyuka <oyuka579@gmail.com>
2022-03-14 19:00:11 +0800

    Time: 1926 ms (5.02%), Space: 43.1 MB (25.63%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var initialValue;
+    const retArray = [];
+    
+    for(a=0; a<= nums.length; a++){
+        initialValue = a;
+        for(i=0; i<= nums.length; i++){
+            if(target-nums[initialValue] == nums[i] && a!=i){
+                retArray.push(initialValue); 
+                //retArray.push();
+            }
+        }
+    }
+    return retArray;
+};
