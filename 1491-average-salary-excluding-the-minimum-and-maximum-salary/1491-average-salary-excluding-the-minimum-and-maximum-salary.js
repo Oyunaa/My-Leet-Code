104306f9194fea48e4edf658a4e44766a25a25de
Oyuka <oyuka579@gmail.com>
2022-05-24 00:04:49 +0800

    Time: 140 ms (5.49%), Space: 41.8 MB (69.88%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    var max = 0;
+    var min = salary[0];
+  
+  
+    for(var i = 0; i < salary.length; i++) {
+      if(max < salary[i]) {
+        max = salary[i];
+      }
+      
+      if(min > salary[i]) {
+        min  = salary[i];
+      }
+    }
+  var sum = 0;
+   for(var i = 0; i < salary.length; i++) {
+      if(min != salary[i] && max != salary[i]) {
+        sum+=salary[i];
+      }
+    }
+  
+  return sum / (salary.length - 2)
+  
+ 
+};
