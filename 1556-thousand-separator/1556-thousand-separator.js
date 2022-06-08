5c24c175d9605424fbe88a8921a30cdd85c9e81f
Oyuka <oyuka579@gmail.com>
2022-06-08 18:21:50 +0800

    Time: 87 ms (31.48%), Space: 42.1 MB (66.67%) - LeetHub

---
+++ 1556-thousand-separator/1556-thousand-separator.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var thousandSeparator = function(n) {
+    var len = n.toString().length;
+
+  if(len <= 3) {
+    return n.toString();
+  }
+  
+  
+  console.log(len % 3)
+  
+  var point = len % 3;
+  var arr = n.toString().split('');
+  var arr1=[];
+  
+  var count = 1;
+  for(var i = arr.length - 1; i >= 0; i--) {
+   // console.log(arr[i])
+    if(count <= 3) {
+      arr1.push(arr[i]);
+      if(count == 3) {
+        if (i != 0 ) { arr1.push('.') }
+        count = 0;
+      }
+      //console.log("arr1",arr1)
+    }
+    
+    count++;
+    
+  }
+  
+  
+  
+  //console.log(arr1.reverse().join(''));
+  
+  
+  
+  return arr1.reverse().join('');
+  
+};
