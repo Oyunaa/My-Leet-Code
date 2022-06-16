0c632d57842ce7624144723db5617800b56ba895
Oyuka <oyuka579@gmail.com>
2022-06-16 23:04:08 +0800

    Time: 62 ms (96.73%), Space: 42.3 MB (69.87%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -3,10 +3,8 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    var a, b;
   var max = 0; prev = 0;
   
-  var len = nums.length;
   var maxindex = 0;
   
   nums.map((i, index)=>{
@@ -24,15 +22,6 @@ var maxProduct = function(nums) {
     }
   })
   
-  console.log(max, prev)
-  
-  
- // nums.sort();
- // console.log(nums.sort())
-  //if(len > 1) {
-   // a = nums[len-1];
-    //b = nums[len-2];
-  //} 
-  
+
   return (max-1) * (prev-1);
 };
