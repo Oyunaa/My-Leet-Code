5a17c4885993ae0fe12ddcc4b41cec87881455b9
Oyuka <oyuka579@gmail.com>
2022-06-20 15:01:45 +0800

    Time: 205 ms (6.99%), Space: 51.6 MB (6.64%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function(words) {
+  
+  var IsFound = false;
+    
+  for(let i = 0; i < words.length; i++) {
+    
+    let aLen = Math.floor(words[i].length / 2);
+    let wLen = words[i].length-1;
+    
+    var countTrue = 0;
+    
+    for(let a = 0; a < aLen; a++) {
+     // console.log(words[i][a])
+      if(words[i][a] == words[i][wLen-a]){
+          countTrue += 1;
+
+      }
+    }
+    
+    
+    console.log(aLen)
+    
+    
+    if(aLen == countTrue) {
+      IsFound = true;
+      return words[i];
+    }
+    
+  }
+  
+  if(IsFound == false) {
+    return "";
+  }
+  
+  
+  
+};
