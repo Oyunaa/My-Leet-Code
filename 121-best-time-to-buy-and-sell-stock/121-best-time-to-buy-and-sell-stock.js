ce78f8328a840530533e6e0184fbebc121e83c8d
Oyuka <oyuka579@gmail.com>
2022-07-04 15:49:25 +0800

    Time: 159 ms (12.81%), Space: 52.1 MB (11.09%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -6,9 +6,11 @@ var maxProfit = function(prices) {
   var profitMax = 0;
   var minPrice = prices[0];
   
-  prices.map(a=>{
+  prices.map((a, i)=>{
+    if(i > 0) {
     minPrice = Math.min(a, minPrice);
     profitMax = Math.max(profitMax, a - minPrice) 
+    }
   })
   
   
