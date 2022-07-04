0edc63f82462f963b487901d5e7e74117bc15301
Oyuka <oyuka579@gmail.com>
2022-07-04 16:49:34 +0800

    Time: 180 ms (25.85%), Space: 42.5 MB (59.22%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,12 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const retArray = [];
     
     for(a=0; a< nums.length; a++){
         for(i=a+1; i< nums.length; i++){
             if(target == nums[i] + nums[a]){
-                retArray.push(a, i); 
+                return [a, i]
             }
         }
     }
