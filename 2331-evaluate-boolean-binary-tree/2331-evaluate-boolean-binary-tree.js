9b327d9567a2d5fdd21a5b73ab80e6f2afc6d344
Oyunjargal <oyuka579@gmail.com>
2022-07-19 21:17:52 +0800

    Time: 114 ms (45.75%), Space: 48.8 MB (38.68%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -19,8 +19,5 @@ var evaluateTree = function(root) {
   if(root.val == 2) {
     return evaluateTree(root.left) || evaluateTree(root.right)
   }
-  
- // if(root.val == 3) {
     return evaluateTree(root.left) && evaluateTree(root.right)
-  //}
 };
