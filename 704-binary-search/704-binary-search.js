aa15a36ee632aec7a819416a84291a338291d87b
Oyunjargal <oyuka579@gmail.com>
2022-07-19 20:35:08 +0800

    Time: 118 ms (15.75%), Space: 45.2 MB (32.03%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,17 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
+    let left = a = 0;
     let right = nums.length-1;
-  let a;
   
     while(left <= right){
       
-      a = (left + (right - left)/2) >> 0; // 2 == nums[2] = 3  //4 == nums[4] = 9
+      a = (left + (right - left)/2) >> 0;
      
-      if(nums[a] == target) return a; // false
+      if(nums[a] == target) return a;
       if(target < nums[a])  right = a - 1; 
-      else  left = a + 1    //left = 3
+      else  left = a + 1;
       
     }
   return -1;
