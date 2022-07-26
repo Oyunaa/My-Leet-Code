161ad74d54801dafaa396f288eb865e78b2a5237
Oyunjargal <oyuka579@gmail.com>
2022-07-26 12:48:10 +0800

    Time: 84 ms (77.57%), Space: 44.2 MB (51.68%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+  
+  if(s==""){
+    return t;
+  }
+  let arr = t.split('');
+  
+  for(char of s) {
+    let indexChar = arr.indexOf(char);
+    if(indexChar!= -1) {
+       arr.splice(indexChar, 1);
+    }
+  }
+  
+  if(arr.length > 0) {
+    return arr[0];
+  }
+};
